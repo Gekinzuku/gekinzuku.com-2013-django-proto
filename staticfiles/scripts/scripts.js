90edb757f18a959a313b317f8d9e041b3be0c736
geekylink <geekyguy2.0@gmail.com>
2013-01-31 22:52:01 -0500

    Login garbage

--- staticfiles/scripts/scripts.js
+++ staticfiles/scripts/scripts.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
   
-  $('#slider').nivoSlider({
+  /* $('#slider').nivoSlider({
   	effect: 'fold',
   	slices: 10,
   	animSpeed: 400,
   	pauseTime: 5000,
   	prevText: '&larr;',
   	nextText: '&rarr;'
-  });
+  }); */
   
-  $("a").mouseover(function() {
+  $("#nav a").mouseover(function() {
     $(this).animate({ color: "#9cddff" }, 400);
     $(this).mouseout(function() {
       $(this).animate({ color: "#ffffff" }, 400);
     });
+  });
+
+  $(function() {
+    $( ".board-new a" ).button();
+    $("input[value='Post']").button({
+      icons: {
+        primary: "ui-icon-locked"
+      }
+    });
+    $(".close").click(function() {
+    	$(".error").remove();
+    })
   });
 
 });
